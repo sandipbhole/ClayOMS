022b69bfc7085edf322f97d57701eea0de70a14f
sandipbhole <sandipbhole@yahoo.com>
2016-10-12 15:53:45 +0530

    Add ArchitUtility tool.

---
+++ Business/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clay.OMS.Data;
+
+namespace Clay.OMS.Business
+{
+    public class FacultyBAL
+    {
+
+    }
+}
