3a56bce9c9d9784eaf0e48fa21f5d8b4bad6de5d
sandipbhole <sandipbhole@yahoo.com>
2016-10-15 11:49:24 +0530

    Added designation message for Designation screen.

---
+++ Message/Designation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Clay.OMS.Message
+{
+    public class Designation
+    {
+        public long designationID { get; set; }
+
+        [Display(Name = "Designation")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Please provide designation.", AllowEmptyStrings = false)]
+        public string designation { get; set; }
+
+        public System.Nullable<Boolean> activated { get; set; }
+
+        public string addUser { get; set; }
+
+        public DateTime addDate { get; set; }
+
+        public string updateUser { get; set; }
+
+        public System.Nullable<DateTime> updateDate { get; set; }
+    }
+}
